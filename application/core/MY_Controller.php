afd2d2e55569d7c6c6a71a0bc6f921086e70a21b
Sai <fdc.samvillarta@gmailcom>
2021-04-11 15:08:04 +0800

    reover

--- application/core/MY_Controller.php
+++ application/core/MY_Controller.php
@@ -1,48 +1,32 @@
 <?php
+defined('BASEPATH') OR exit('No direct script access allowed');
 
-class MY_Router extends CI_Router {
-    protected function _set_default_controller() {
+class My_Controller extends CI_Controller {
 
-        if (empty($this->default_controller)) {
+	//
+	public function pageRenderer($page, array $data){
+		$this->load->view('includes/landingPage/header',$data);
+		$this->load->view($page);
+		$this->load->view('includes/landingPage/footer');
+	}
 
-            show_error('Unable to determine what should be displayed. A default route has not been specified in the routing file.');
-        }
-        // Is the method being specified?
-        if (sscanf($this->default_controller, '%[^/]/%s', $class, $method) !== 2) {
-            $method = 'index';
-        }
+	//landing pages
+	public function landingPageRenderer($page, array $data){
+		$this->load->view('includes/landingPage/header',$data);
+		$this->load->view($page);
+		$this->load->view('includes/landingPage/footer');
+	}
 
-        // This is what I added, checks if the class is a directory
-        if( is_dir(APPPATH.'controllers/'.$class) ) {
+	
+	public function registrarPageRenderer($page, array $data){
+		$this->load->view('includes/registrar/header',$data);
+		$this->load->view($page);
+		$this->load->view('includes/registrar/footer');
+	}
 
-            // Set the class as the directory
-
-            $this->set_directory($class);
-
-            // $method is the class
-
-            $class = $method;
-
-            // Re check for slash if method has been set
-
-            if (sscanf($method, '%[^/]/%s', $class, $method) !== 2) {
-                $method = 'index';
-            }
-        }
-
-        if ( ! file_exists(APPPATH.'controllers/'.$this->directory.ucfirst($class).'.php')) {
-
-            // This will trigger 404 later
-
-            return;
-        }
-        $this->set_class($class);
-        $this->set_method($method);
-        // Assign routed segments, index starting from 1
-        $this->uri->rsegments = array(
-            1 => $class,
-            2 => $method
-        );
-        log_message('debug', 'No URI present. Default controller set.');
-    }
+	public function studentPageRendere($page, array $data){
+		$this->load->view('includes/student/header',$data);
+		$this->load->view($page);
+		$this->load->view('includes/student/footer');
+	}
 }
