227cc972e4fc3c6176f0d0a4186f83eb1f7ae81c
Sai <fdc.samvillarta@gmailcom>
2021-05-08 22:55:50 +0800

    Sai - add env 2.2

--- index.php
+++ index.php
@@ -63,9 +63,9 @@
  * Different environments will require different levels of error reporting.
  * By default development will show errors but testing and live will hide them.
  */
-switch (ENVIRONMENT)
+switch ($_SERVER["HTTP_HOST"])
 {
-	case 'development':
+	case 'localhost':
 		error_reporting(-1);
 		ini_set('display_errors', 1);
 	break;
@@ -84,9 +84,15 @@ switch (ENVIRONMENT)
 	break;
 
 	default:
-		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
-		echo 'The application environment is not set correctly.';
-		exit(1); // EXIT_ERROR
+        ini_set('display_errors', 0);
+        if (version_compare(PHP_VERSION, '5.3', '>='))
+        {
+            error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
+        }
+        else
+        {
+            error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
+        }
 }
 
 /*
