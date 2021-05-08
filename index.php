06e1d3c9373f2fcb8505eaf3542c7521d750bb24
Sai <fdc.samvillarta@gmailcom>
2021-05-08 23:12:09 +0800

    Sai

--- index.php
+++ index.php
@@ -63,19 +63,14 @@
  * Different environments will require different levels of error reporting.
  * By default development will show errors but testing and live will hide them.
  */
-switch ($_SERVER["HTTP_HOST"])
+switch (ENVIRONMENT)
 {
-	case 'localhost':
+	case 'development':
 		error_reporting(-1);
 		ini_set('display_errors', 1);
 	break;
 
-	case 'capstone-online.herokuapp.com':
-        // Report all errors except E_NOTICE
-        // This is the default value set in php.ini
-        error_reporting(E_ALL ^ E_NOTICE);
-        ini_set('display_errors', 0);
-    break;
+	case 'testing':
 	case 'production':
 		ini_set('display_errors', 0);
 		if (version_compare(PHP_VERSION, '5.3', '>='))
