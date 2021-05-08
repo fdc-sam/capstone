87e5a06b15a89dfdb182e831d4b95b5b6e45e482
Sai <fdc.samvillarta@gmailcom>
2021-05-08 22:59:47 +0800

    Sai - remove env

--- index.php
+++ index.php
@@ -84,15 +84,9 @@ switch ($_SERVER["HTTP_HOST"])
 	break;
 
 	default:
-        ini_set('display_errors', 0);
-        if (version_compare(PHP_VERSION, '5.3', '>='))
-        {
-            error_reporting(E_ALL & ~E_NOTICE & ~E_DEPRECATED & ~E_STRICT & ~E_USER_NOTICE & ~E_USER_DEPRECATED);
-        }
-        else
-        {
-            error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT & ~E_USER_NOTICE);
-        }
+		header('HTTP/1.1 503 Service Unavailable.', TRUE, 503);
+		echo 'The application environment is not set correctly.';
+		exit(1); // EXIT_ERROR
 }
 
 /*
