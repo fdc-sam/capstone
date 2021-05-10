16a82a3f767eae12465fbf347ae52c535e7829a9
Sai <fdc.samvillarta@gmailcom>
2021-05-10 15:10:57 +0800

    Sai - to unlock email error

--- application/views/includes/landingPage/footer.php
+++ application/views/includes/landingPage/footer.php
@@ -1,6 +1,6 @@
 </body>
 </html>
-<script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
+<script src="<?php echo base_url('assets/scripts/jquery-3.6.0.js'); ?>"></script>
 <script type="text/javascript" src="<?php echo base_url('assets/scripts/main.d810cf0ae7f39f28f336.js'); ?>"></script>
 
 <script type="text/javascript">
